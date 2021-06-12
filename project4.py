93c0a7fd7def22cdcff743f68a3b7fa91111fd32
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2021-06-12 20:03:58 +0900

    final 6.12

--- project4.py
+++ project4.py
@@ -1,3 +1,4 @@
+#Class Definition
 class Cafe:
     def __init__(self,name="",quantity=0):
         self._name = name
@@ -22,12 +23,14 @@ class Food(Cafe):
         set._isWarm=isWarm
     def getIsWarm(self):
         return self._isWarm
+
 def main():
     listOfMenus=[]
     listOfNames=[]
     listOfQuantity=[]
     carryOn='Y'
     while carryOn=='Y':
+#Receive a name for the menu
         nameOn='Y'
         while nameOn=='Y':
             name=input("Enter menu's name: ")
@@ -36,6 +39,7 @@ def main():
                 continue
             else:
                 nameOn='N'
+#Receive quantity for the menu
                 quantityOn='Y'
                 while quantityOn=='Y':
                     try:
@@ -45,28 +49,31 @@ def main():
                         continue
                     else:
                         quantityOn='N'
+#Receive category for the menu
                         categoryOn='Y'
                         baseCategory={"DRINK":"DRINK","drink":"drink","FOOD":"FOOD","food":"food"}
                         while categoryOn=='Y':
                             try:
-                                category = input("Enter category of menu(DRINK/FOOD) ")
+                                category = input("Enter category of menu(DRINK/FOOD): ")
                                 category = baseCategory[category]
                             except:
-                                print("You have to enter drink or food")
+                                print("You have to enter 'drink' or 'food'.")
                                 continue
                             else:
                                 categoryOn='N'
+#Category of Drink 
                                 if category.upper()=="DRINK":
                                     st = Drink(name,quantity)
                                 else:
+#Category of Food and asking warming option for the menu
                                     questionOn='Y'
                                     baseQuestion={"WARM":"WARM","warm":"warm","COLD":"COLD","cold":"cold"}
                                     while questionOn=='Y':
                                         try:
-                                            question=input("Enter warming option(WARM/COLD)")
+                                            question=input("Enter warming option(WARM/COLD): ")
                                             question=baseQuestion[question]
                                         except:
-                                            print("You have to enter warm or cold.")
+                                            print("You have to enter 'warm' or 'cold'.")
                                             continue
                                         else:
                                             questionOn='N'
@@ -75,26 +82,32 @@ def main():
                                             else:
                                                 isWarm=False
                                                 st=Food(name,quantity,isWarm) 
+#Making lists
         listOfMenus.append(st)
         listOfNames.append(st._name)
         listOfQuantity.append(st._quantity)
+#Asking continuing this menu input process
         while True:
-            carryOn=input("Do you wnat to continue?(Y or N)")
-            if carryOn!='Y' and carryOn!='N':
-                print("You have to enter Y or N.")
+            carryOn=input("Do you wnat to continue?(Y or N): ")
+            if carryOn!='Y' and carryOn!='N' and carryOn!='y' and carryOn!='n':
+                print("You have to enter 'Y' or 'N'.")
                 continue
             else:
                 carryOn=carryOn.upper()
                 break
+#Starting selecting process by using selecting definition
     selecting(listOfNames,listOfQuantity)
+#Definition for selecting process
 def selecting(listOfNames,listOfQuantity):
+#Displaying menu
     while True:
-        print("*"*47)
-        print("Welcome to Hello cafe")
-        print("*"*47)
+        print("*"*20)
+        print("{0:^20s}".format("Hello Cafe"))
+        print("*"*20)
         for i in range(len(listOfNames)):
             print(i+1,")",listOfNames[i],"(quantity:",listOfQuantity[i],")")
         selectMenuOn='Y'
+#Receive mnumber of menu by customer
         while selectMenuOn=='Y':
             try:
                 selectMenu=int(input("Enter menu number: "))-1
@@ -103,13 +116,14 @@ def selecting(listOfNames,listOfQuantity):
                 continue
             else:
                 if selectMenu > len(listOfNames)-1:
-                    print("There is only",len(listOfNames),"numbers in menu.")
+                    print("The number only exists up to",len(listOfNames),".")
                 else:
                     selectMenuOn='N'
+#Receive quantity of menu by customer
                     selectQuantityOn='Y'
                     while selectQuantityOn=='Y':
                         try:
-                            selectQuantity=int(input("Enter quantity"))
+                            selectQuantity=int(input("Enter quantity: "))
                         except:
                             print("You have to enter number.")
                             continue
@@ -119,13 +133,16 @@ def selecting(listOfNames,listOfQuantity):
                                 continue
                             else:
                                 break
+#When deleting menu and cafe closing
         listOfQuantity[selectMenu] -= selectQuantity
         if listOfQuantity[selectMenu]==0:
             print("no left. delete " + listOfNames[selectMenu])
             del listOfNames[selectMenu]
             del listOfQuantity[selectMenu]
             if len(listOfNames)==0:
-                print("All sold out")
+                print("#"*47)
+                print("All sold out. Thank you for using \"Hello Cafe\".")
+                print("#"*47)
                 break
             else:
                 continue
